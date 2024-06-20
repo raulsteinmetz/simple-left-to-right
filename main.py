b8e5f70964f7d2ca4d6f3917c534a2bd50dafde6
raulsteinmetz <rsteinmetz@inf.ufsm.br>
2024-06-20 13:58:51 -0300

    specifications

---
+++ main.py
@@ -0,0 +1,12 @@
+# gramatica que reconheça gramaticas regulares
+# interpretdor de gramatica
+
+# gramaticas tem que ter os 4 elementos da definição formal
+
+# rodar a gramatica 1 COM uma gramatica 2 (string) 
+# pegar a gramatica 2 em forma de codigo
+# ler palavra do terminal e reconhecer usando a gramatica 2
+# retorna arvore de derivação
+# caso de erro tem que ter a posição do erro
+
+# todas as gramaticas são GLD
