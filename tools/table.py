166a4bc4c66c50dc1bc7f363f31f8581760cc6e7
raulsteinmetz <rsteinmetz@inf.ufsm.br>
2024-06-30 09:36:26 -0300

    rules for first and follow

--- tools/table.py
+++ tools/table.py
@@ -1,10 +1,20 @@
 # firsts = get_firsts(grammar) # TODO: not working fully
 
 def get_firsts(grammar: dict):
+    '''
+        if x is terminal, First(x) = {x},
+        if X -> eps, eps in First(X),
+        if X -> Y[...] and Y is non terminal, First(Y) in First(X)
+    '''
     pass
 
 
 def get_follows(grammar:dict):
+    '''
+        $ in follow(S) if S is the initial symbol,
+        if A -> [..]XB, First(B) in Follow(X) - b being terminal or not,
+        if A -> [...]X or A -> [...]XB and B -> eps, Follow(A) in Follow(X) 
+    '''
     pass
 
 
