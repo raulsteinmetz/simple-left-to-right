d46dee319d34a1e8a4e0f5b22341464601dec339
raulsteinmetz <rsteinmetz@inf.ufsm.br>
2024-07-10 07:53:06 -0300

    new parameter for verifying gld or not

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import argparse
 import ast
 
-from lexer.grammar import read_txt, parse_yaml, nice_grammar_print
+from lexer.grammar import read_txt, parse_yaml, nice_grammar_print, grammar_to_string
 from lexer.lexer import tokenize_grammar, tokenize_word
 from slr.slr import run_slr
 
-def main(grammar_path: str, words_path: str):
+def main(grammar_path: str, checkrl: bool):
 
     # creates dictionary for right linear grammar recognizer grammar
     grammar_recognizer = parse_yaml('./grammars/gld.yaml')
 
+    grammar = ''
     # grammars can either be on txt or yaml
     if grammar_path.endswith('.yaml'):
-        grammar_dict = parse_yaml(grammar_path)
+        # parse from file
+        grammar = grammar_to_string(parse_yaml(grammar_path)).replace('"', "'")
     elif grammar_path.endswith('.txt'):
+        # parse from file
         grammar = read_txt(grammar_path)
+    else:
+        print('File format for Grammar definition not supported')
         
+    if checkrl:
         # verify if its a right linear grammar
         grammar_tokens = tokenize_grammar(grammar)
         # recognize grammar structure
         if not run_slr(grammar_recognizer, grammar_tokens):
             print('Grammar not recognized as a right-linear grammar')
             exit()
-        
         print('Grammar is a correctly defined right-linear grammar.')
-        # turn into dictionary for evaluating words
-        grammar_dict = ast.literal_eval(grammar)
+
+    # turn into dictionary for evaluating words
+    grammar_dict = ast.literal_eval(grammar)
 
 
     nice_grammar_print(grammar_dict)
@@ -45,7 +51,7 @@ def main(grammar_path: str, words_path: str):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Recognize a grammar and test words on it')
     parser.add_argument('--grammar', type=str, default='./grammars/ops.yaml', help='Path to your grammar definition')
-    parser.add_argument('--words', type=str, default='TODO', help='Path to the words to be tested on your grammar')
+    parser.add_argument('--checkrl', type=bool, default=True, help='Check if grammar is right-linear')
     args = parser.parse_args()
     
-    main(args.grammar, args.words)
+    main(args.grammar, args.checkrl)
